33800aa450e9625cf75308ba379fa6b1e5c1c49b
DevPrince3 <princekumar454500@gmail.com>
2026-09-25 12:16:28 +0530

    Add files via upload
    casting can't be done on array

---
+++ casting.java
@@ -0,0 +1,9 @@
+package Arrays;
+
+public class casting {
+    static void main() {
+        int[] integers={1,2,3};
+        double decimals=(double[])integers;
+        System.out.println(decimals[0]);
+    }
+}
