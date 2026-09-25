7ad1e7d126ee9ad208d7e07f696236128a14cbe3
DevPrince3 <princekumar454500@gmail.com>
2026-09-25 12:01:14 +0530

    1st class of java

---
+++ Test.java
@@ -0,0 +1,24 @@
+package FirstClass;
+
+//public class Test {
+//    static void main() {
+//        double a=295.04;
+//        byte c=(byte)a;
+//        System.out.println(c);
+//    }
+//}
+//public class Test {
+//    static void main() {
+//        char ch='b';
+//        int num=(ch+2);
+//        System.out.println((char)num);
+//    }
+//}
+public class Test {
+    static void main() {
+        short a=10;
+        short b=20;
+        short c= (short) (a+b);
+        System.out.println(c);
+    }
+}
